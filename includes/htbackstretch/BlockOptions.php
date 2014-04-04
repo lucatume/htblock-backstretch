587630cbdd0bc1c2eb28b0767d26aa2d1432f51c
Luca Tumedei <luca@theaveragedev.com>
2014-04-04 09:45:05 +0200

    removed the unused block options (there's no block at all)

--- includes/htbackstretch/BlockOptions.php
+++ includes/htbackstretch/BlockOptions.php
@@ -1,22 +1,23 @@
 <?php
 namespace htbackstretch;
 
-    class BlockOptions extends \HeadwayBlockOptionsAPI {
+class BlockOptions extends \HeadwayBlockOptionsAPI
+{
 
-        public $tabs = array(
-            'my-only-tab' => 'Settings'
+    public $tabs = array(
+            // 'my-only-tab' => 'Settings'
         );
 
-        public $inputs = array(
+    public $inputs = array(
 
-            'my-only-tab' => array(
+            // 'my-only-tab' => array(
 
-                'text-input' => array(
-                    'type' => 'image',
-                    'name' => 'logo',
-                    'label' => 'Logo',
-                    'tooltip' => 'Put anything relevant about the input in this tooltip.'
-                )
-            )
+            //     'text-input' => array(
+            //         'type' => 'image',
+            //         'name' => 'logo',
+            //         'label' => 'Logo',
+            //         'tooltip' => 'Put anything relevant about the input in this tooltip.'
+            //     )
+            // )
         );
-    }
+}
